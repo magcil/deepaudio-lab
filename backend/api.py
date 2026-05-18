f419da5ba1108e6d9f5eca2aba4a9fc820d28536
ChrisNick92 <chrisnick92@gmail.com>
2026-05-18 12:15:00 +0300

    (issue #50) modules initialization for handling storage, client, bussiness logic; endpoint for uploading dataset

--- backend/api.py
+++ backend/api.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from db.session import Base, engine
 from exceptions.handlers import register_exception_handlers
-from routers import evaluation, run, tasks, training
+from routers import evaluation, run, tasks, training, storage
 
 # Instantiate api
 app = FastAPI()
@@ -26,6 +26,7 @@ app.include_router(training.router)
 app.include_router(evaluation.router)
 app.include_router(run.router)
 app.include_router(tasks.router)
+app.include_router(storage.router)
 
 
 # Check status point
